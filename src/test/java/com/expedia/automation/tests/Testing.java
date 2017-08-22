4dd993020970115675c1af8ccb171de287506c7a
Vlad <vladislav.baranovski@gmail.com>
2017-08-22 18:57:55 +0300

    Update

--- src/test/java/com/expedia/automation/tests/Testing.java
+++ src/test/java/com/expedia/automation/tests/Testing.java
@@ -2,6 +2,7 @@ package com.expedia.automation.tests;
 
 import com.expedia.automation.browser.Browser;
 import com.expedia.automation.pages.navigation.account.HeaderMenuAccount;
+import com.expedia.automation.pages.search.flights.FlightsTab;
 import com.expedia.automation.pages.signin.SignInPage;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
@@ -18,7 +19,10 @@ public class Testing {
     private static final String EMAIL = "vladislav.baranovski1@gmail.com";
     private static final String PASSWORD = "barvadya18";
 
-
+    private static final String FLYINGFROM = "Minsk, Belarus (MSQ-Minsk Intl.)";
+    private static final String FLYINGTO = "New York, NY (NYC-All Airports)";
+    private static final String DATEDEPART = "09/25/2017";
+    private static final String DATERETURN = "09/30/2017";
 
 
     @BeforeTest
@@ -27,22 +31,33 @@ public class Testing {
         return driver;
     }
 
-    @Test (priority = 1)
+    @Test (enabled = true, priority = 1)
     public void openHomePage(){
        Browser homePage = new Browser();
        homePage.openExpedia(driver);
     }
 
-    @Test (priority = 2)
+    @Test (enabled = true, priority = 2)
     public void openSignInScreen(){
+        Browser homePage = new Browser();
+        homePage.openExpedia(driver);
         HeaderMenuAccount signIn = new HeaderMenuAccount(driver);
         signIn.openSignInScreen(driver);
     }
 
-    @Test (priority = 3)
+    @Test (enabled = true, priority = 3, dependsOnMethods = "openSignInScreen")
     public void loginTo(){
         SignInPage sign = new SignInPage(driver);
         sign.signIn(driver,EMAIL, PASSWORD);
+    }
+
+
+    @Test (priority = 4)
+    public void searchFlightCheck(){
+        Browser homePage = new Browser();
+        homePage.openExpedia(driver);
+        FlightsTab flights = new FlightsTab(driver);
+        flights.searchFligthWithCar(FLYINGFROM, FLYINGTO, DATEDEPART, DATERETURN, 2);
     }
 
     @AfterTest
